faf6363ecd7579b62d522cb8610d52e44a1134c2
hercules158 <hercules.corcini@gmail.com>
2021-12-15 20:10:44 -0300

    Criação das duas primeiras telas

--- app/src/main/java/br/com/gerencianet/tabelafipe/MainActivity.kt
+++ app/src/main/java/br/com/gerencianet/tabelafipe/MainActivity.kt
@@ -2,10 +2,15 @@ package br.com.gerencianet.tabelafipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.gerencianet.tabelafipe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/br/com/gerencianet/tabelafipe/StartFragment.kt
@@ -0,0 +1,37 @@
+package br.com.gerencianet.tabelafipe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.com.gerencianet.tabelafipe.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+
+ lateinit var binding: FragmentStartBinding
+ val controller by lazy { findNavController() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.bNext.setOnClickListener{
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentStartBinding.inflate(inflater,container,false)
+        return binding.root
+
+    }
+
+}
